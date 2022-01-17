3656596accb77646267a9a8a47896086db883d23
Joe Daddario <joe@shrthnd.org>
2022-01-17 15:05:34 -0500

    Improve game mechanics

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const SPACER = 25; 
+const FPS = 15; 
+let TIMESTAMP = 0; 
 
 const Vector = (x, y) => {
-    return {
-        x: x,
-        y: y
-    }
+  return {
+    x: x,
+    y: y
+  }
 }
 
 const player = {
@@ -20,31 +22,87 @@ const player = {
     // draw remaining nodes
     let j = 0; 
     for (let i = world.player.body.length - 1; i > -1; i--) {
-        if (j >= world.player.maxLength) {
-            break;
-        }
-        context.fillRect(world.player.body[i].x, world.player.body[i].y, SPACER, SPACER); 
-        j++;      
+      if (j >= world.player.maxLength) {
+        break;
+      }
+      context.fillRect(world.player.body[i].x, world.player.body[i].y, SPACER, SPACER); 
+      j++;      
     }
 
     // draw chips
     for (let i = 0; i < world.chips.length; i++) {
-        context.fillStyle = 'darkblue'; 
-        context.fillRect(world.chips[i].x, world.chips[i].y, SPACER, SPACER); 
+      context.fillStyle = 'darkblue'; 
+      context.fillRect(world.chips[i].x, world.chips[i].y, SPACER, SPACER); 
     }
+
+    // paint context
     context.fill();
+
+    // check collision with chips
+    for (let i = 0; i < world.chips.length; i++) {
+      if (
+        world.chips[i].x == world.player.position.x &&
+        world.chips[i].y == world.player.position.y
+      ) {
+        world.player.maxLength++;
+        world.chips.splice(i, 1); 
+        for (let i = 0; i < 2; i++) {
+          world.chips.push(
+            Vector(
+              10 + SPACER * (Math.floor(Math.random() * Math.floor((world.canvas.width - 10) / SPACER))), 
+              10 + SPACER * (Math.floor(Math.random() * Math.floor((world.canvas.height - 10) / SPACER)))
+            )
+          );
+        }
+      }
+    }
+
+    // check collision with self 
+    for (let i = world.player.body.length - 1; i > 0; i--) {
+      if (
+        world.player.position.x == world.player.body[i].x &&
+        world.player.position.y == world.player.body[i].y
+      ) {
+        world.playing = false; 
+        canvas.style.backgroundColor = 'red'; 
+      }
+    }
+
+    const nextPositionX = world.player.position.x + (world.player.velocity.x * SPACER);
+    const nextPositionY = world.player.position.y + (world.player.velocity.y * SPACER); 
+    if (
+      nextPositionX < world.canvas.width - SPACER &&
+      nextPositionY < world.canvas.height - SPACER && 
+      nextPositionX > 0 &&
+      nextPositionY > 0
+    ) {
+      world.player.body.push({
+        x: world.player.position.x, 
+        y: world.player.position.y
+      });
+
+      world.player.body = world.player.body.slice(
+        world.player.body.length - world.player.maxLength, 
+        world.player.body.length
+      );
+      
+      world.player.position.x = nextPositionX;
+      world.player.position.y = nextPositionY;
+    }
+    world.keypressed = false; 
   }
 }
 
 // game world
 const world = {
-    canvas: {
-        width: window.innerWidth - 1,
-        height: window.innerHeight - 4,
-    },
-    keypressed: false,
-    chips: [Vector(10+SPACER*2, 10+SPACER*2)], 
-    player: player
+  playing: true, 
+  canvas: {
+    width: window.innerWidth - 1,
+    height: window.innerHeight - 4,
+  },
+  keypressed: false,
+  chips: [Vector(10+SPACER*2, 10+SPACER*2)], 
+  player: player
 }
 
 // create canvas
@@ -59,84 +117,55 @@ canvas.width = world.canvas.width;
 canvas.height = world.canvas.height;
 body.append(canvas);
 
-
 // keyboard and mouse events
-
 const handleKeys = (e) => {
     const KEY = e.key;
     switch (KEY) {
+      case ' ':
+        world.player.position = Vector(10,10);
+        world.player.velocity = Vector(0,0);
+        world.player.body = []; 
+        world.player.maxLength = 0; 
+        world.chips = [Vector(10+SPACER*2, 10+SPACER*2)]; 
+        canvas.style.backgroundColor = 'blue'; 
+        world.playing = true; 
+        break;
       case 'ArrowUp':
-        world.player.position.y -= SPACER;
+        if (world.player.velocity.y != 1) {
+          world.player.velocity = Vector(0, -1);
+        }
         world.keypressed = true; 
         break;
       case 'ArrowDown':
-        world.player.position.y += SPACER;
+        if (world.player.velocity.y != -1) {
+          world.player.velocity = Vector(0, 1);
+        }
         world.keypressed = true; 
         break;
       case 'ArrowLeft':
-        world.player.position.x -= SPACER;
+        if (world.player.velocity.x != 1) {
+          world.player.velocity = Vector(-1, 0);
+        }
         world.keypressed = true; 
         break;
       case 'ArrowRight':
-        world.player.position.x += SPACER;
+        if (world.player.velocity.x != -1) {
+          world.player.velocity = Vector(1, 0);
+        }
         world.keypressed = true; 
         break;
       default: 
-    }
-    if (world.keypressed) {
-
-      // check collision with chips
-      if (world.chips.length) {
-        for (let i = 0; i < world.chips.length; i++) {
-          if (
-            world.chips[i].x == world.player.position.x &&
-            world.chips[i].y == world.player.position.y
-          ) {
-            world.player.maxLength++;
-            world.chips.splice(i, 1); 
-            for (let i = 0; i < 2; i++) {
-              world.chips.push(
-                Vector(
-                  10 + SPACER * (Math.floor(Math.random() * Math.floor((world.canvas.width - 10) / SPACER))), 
-                  10 + SPACER * (Math.floor(Math.random() * Math.floor((world.canvas.height - 10) / SPACER)))
-                )
-              );
-            }
-          }
-        }
-      }
-
-      // check collision with self 
-      for (let i = world.player.body.length - 1; i >= 0; i--) {
-        if (
-          world.player.position.x == world.player.body[i].x &&
-          world.player.position.y == world.player.body[i].y
-        ) {
-          canvas.style.backgroundColor = 'red'; 
-        }
-      }
-
-      world.player.body.push({
-        x: world.player.position.x, 
-        y: world.player.position.y
-      });
-
-      world.player.body = world.player.body.slice(
-        world.player.body.length-world.player.maxLength, 
-        world.player.body.length
-      );
-
-      world.keypressed = false; 
     }
 }
 window.addEventListener('keydown', handleKeys);
 
 // game loop
-
-const draw = () => {
+const draw = (now) => {
+  requestAnimationFrame(draw);
+  if (now - TIMESTAMP < 1000 / FPS || !world.playing) return; 
   context.clearRect(0, 0, world.canvas.width, world.canvas.height);
   world.player.draw(world);
-  requestAnimationFrame(draw);
+  TIMESTAMP = now;
 }
 
 draw(); 
