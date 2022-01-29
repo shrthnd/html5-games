9c28a42c1cfeeee551d52f6982e2cdca49c202e1
Joe Daddario <joe@shrthnd.org>
2022-01-29 13:10:56 -0500

    Randomize tetrimino generation; add (p)lay/pause+(r)eset;

--- tetris-clone/tetris.js
+++ tetris-clone/tetris.js
@@ -1,28 +1,67 @@
+const Utils = {
+  largest: (array) => {
+    let max = { index: 0, value: array[0] };
+    for (let index = 0; index < array.length; index++) {
+      const value = array[index];  
+      if (value > max.value) {
+        max = { index, value };
+      }
+    }
+    return max; 
+  }
+}
+
 const Tetris = () => {
     const body = document.body; 
     const canvas = document.createElement('canvas');
     const context = canvas.getContext('2d'); 
-    const fps = 1000;
+    const fps = 60;
     const scale = 20; 
+    let playing = true;
     let timestamp = 0;
     let ticker = 0; 
 
     const heap = {
-      x: [0, 1],
-      y: [0, 1]
+      x: [0, 1, 2, 2],
+      y: [0, 0, 0, 1]
     }
-    
-    const tetrimino = {
-      x: [0, 0, 0, 1],
-      y: [0, 1, 2, 2]
+
+    const tetrimino = () => {
+      console.log("generating tetrimino"); 
+      const xmino = [
+        [0,0,0,0],
+        [0,1,1,2],
+        [0,1,2,2],
+        [0,0,1,2],
+        [0,0,1,1]
+      ];
+      const ymino = [
+        [0,1,2,3],
+        [0,0,1,0],
+        [0,0,0,1],
+        [1,0,0,0],
+        [1,0,1,0]
+      ];
+      const s = Math.floor(Math.random() * xmino.length); 
+      const t = {
+        x: xmino[s],
+        y: ymino[s]
+      }; 
+      console.log(t);
+      return t; 
     }
 
     const player = {
       x: 0,
       y: 0,
-      t: tetrimino
+      t: tetrimino()
     }
 
+    const pixel = (color, position) => {
+      context.fillStyle = color; 
+      context.fillRect(position[0], position[1], position[2], position[3]);
+    }
+    
     const handleControls = (e) => {
       switch(e.key) {
         case "ArrowUp":
@@ -30,8 +69,8 @@ const Tetris = () => {
           ticker = ticker - scale;
           break;
         case "ArrowDown":
-          player.y = player.y + scale; 
-          ticker = ticker + scale; 
+          player.y = player.y + scale;
+          ticker = ticker + scale;
           break;
         case "ArrowLeft":
           if (player.x > 0) {
@@ -40,64 +79,75 @@ const Tetris = () => {
           break;
         case "ArrowRight":
           const boundary = player.x + player.t.x[player.t.x.length-1] * scale; 
-          if (boundary < canvas.width - (scale * 2) ) {
+          if (boundary < canvas.width - scale * 2) {
             player.x = player.x + scale;
           }
-          break; 
+          break;
+        case "P":
+        case "p":
+          // set play/pause
+          playing = !playing; 
+          break;
+        case "R":
+        case "r":
+          resetPlayerState();
+          context.clearRect(0, 0, canvas.width, canvas.height);
+          updatePlayer();  
+          updateHeap();  
+          break;
         default:
           break;
       }
     }
-
-    const pixel = (color, position) => {
-      context.fillStyle = color; 
-      context.fillRect(position[0], position[1], position[2], position[3]);
-    }
-
         
     const resetPlayerState = () => {
       player.y = 0;
       player.x = 0;
-      player.t = tetrimino; 
-      ticker = 0; 
+      player.t = tetrimino(); 
+      ticker = 0;
     }
     
     const checkCollision = () => {
-      const heapHeight = heap.y[heap.y.length-1] * scale;
-      const heapOffsetY = canvas.height - heapHeight; 
+      const y = player.t.y;
+      const x = player.t.x;
       const playerHeight = player.t.y[player.t.y.length-1] * scale;
-      const playerOffsetY = playerHeight + player.y; 
-      
-      if (playerOffsetY >= heapOffsetY) {
-        console.log("collision!");
-      }
-        
+      const playerOffsetY = playerHeight + player.y;
+      const largestX = Utils.largest(x); 
+      const largestY = Utils.largest(y); 
+      console.log("X: ", largestX.value, y[largestX.index]); 
+      console.log("Y: ", x[largestY.index], largestY.value); 
+      console.log("...");
       if (playerOffsetY >= canvas.height) {
-        console.log("collision with floor!");
-        // heap.x.push(player.t.x); 
-        // heap.y.push(player.t.y); 
-        // console.log(heap);
+        for (let i = 0; i < player.t.x.length; i++) {
+          const playerX = player.t.x[i];
+          const playerY = player.t.x[i];
+          for (let j = 0; j < heap.x.length; j++) {
+            const heapX = heap.x[j];
+            const heapY = heap.y[j];
+          }
+        }
         resetPlayerState(); 
       }
     }
 
     const updateHeap = () => {
       for(var i = 0; i < heap.x.length; i++) {
-        // console.log(i, heap.x[i]);
         const x = heap.x[i] * scale;
-        const y = canvas.height - scale;
-        pixel('orange', [x, y, scale, scale]);
+        const y = scale * (heap.y[i] + 1);
+        pixel('orange', [x, canvas.height - y, scale, scale]);
       }
     }
 
-    const updatePlayer = () => {
+    const updatePlayer = (now) => {
+      checkCollision(); 
+
+      // draw player
       for(var i = 0; i < player.t.x.length; i++) {
         const x = player.x + player.t.x[i] * scale;
         const y = (player.t.y[i] * scale) + ticker;
         player.y = y;
         pixel('blue', [x, y, scale, scale]); 
       }
-      checkCollision(); 
     }
 
     const setWindowSize = () => {
@@ -105,23 +155,26 @@ const Tetris = () => {
       canvas.height = window.innerHeight;
     }
 
+    const paint = (now) => {
+      requestAnimationFrame(paint);
+      if (playing) {
+        // if (now - timestamp < 1000 / fps) return; 
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        updatePlayer();
+        updateHeap();
+        ticker += 1;
+        // timestamp = now;
+      }
+    }
+    
     const init = () => {
       setWindowSize();
       window.addEventListener('keydown', handleControls); 
       window.addEventListener('resize', setWindowSize);
-      body.style = "overflow: hidden;";
+      body.style.overflow = "hidden";
+      canvas.style.backgroundColor = "#f5f5f5";
       body.append(canvas);
-      paint(); 
-    }
-
-    const paint = (now) => {
-      requestAnimationFrame(paint);
-      // if (now - timestamp < 2000 / fps) return; 
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      updatePlayer();
-      updateHeap();
-      ticker += 1;
-      // timestamp = now;
+      paint();
     }
  
     init(); 
